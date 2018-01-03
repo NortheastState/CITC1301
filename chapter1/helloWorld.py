176befa81005eee2cea8e83367c247526698eba5
David Blair <dlblair@northeaststate.edu>
2018-01-03 13:04:07 -0500

    Initial Commit for helloWorld in chapter 1

---
+++ chapter1/helloWorld.py
@@ -0,0 +1,15 @@
+#===============================================================
+#
+#        Name: David Blair
+#        Date: 01/01/2018
+#      Course: CITC 1301
+#     Section: A70
+# Description: It is customary to output "Hello World" when
+#              learning a new language. This program does one
+#              thing, and that is output "Hello World!"
+#
+#===============================================================
+
+#print is a python method that outputs text to the console window
+print("Hello World")
+
