330e9f909105e8d684ec3d1d089a0ac0e20dc317
David Blair <dlblair@northeaststate.edu>
2018-01-20 13:57:18 -0500

    Initial Commit for chapter 1 foobar

---
+++ chapter1/foobar.py
@@ -0,0 +1,25 @@
+# ===============================================================
+#
+#        Name: David Blair
+#        Date: 01/01/2018
+#      Course: CITC 1301
+#     Section: A70
+# Description: In the program we will examine simple output
+#              using a function definition with no parameters
+#              and a print statement.
+#              Note that Python does not require a main function,
+#              even though there is one behind the curtains you
+#              you can't see, but it is customary to use one anyway.
+#              Also, we will see later in the course that getting
+#              use to using one will be a good thing to do.
+#
+# ===============================================================
+
+# define the main function that takes no parameters.
+# functions are not called when the program executes, instead it
+# called only by name underneath.
+def main():
+    print("Hello Mr. Foobar!")
+
+# make a call to the main() function above
+main()
