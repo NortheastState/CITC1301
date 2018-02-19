f3992771276841121d44c8ec8c660ce57826f59b
David Blair <dlblair@northeaststate.edu>
2018-02-19 05:03:54 -0500

    initial commit

---
+++ chapter7/exceptionsExample.py
@@ -0,0 +1,37 @@
+# ===============================================================
+#
+#        Name: David Blair
+#        Date: 01/01/2018
+#      Course: CITC 1301
+#     Section: A70
+# Description: This program shows how to handle program exceptionss
+#              for expected and unexpected input values.
+#
+# ===============================================================
+
+
+def main():
+
+    try:
+
+        var1 = int(input("Enter an integer value: "))
+        var2 = var1 / 0
+        print(var2)
+
+    except NameError as err:
+        print("Value was not a number!")
+
+    except ValueError as err:
+        print("Value error, oops")
+
+    except ZeroDivisionError as err:
+        print("There was a divide by zero error!")
+
+    except:
+        print("Something else happened")
+
+
+if __name__ == '__main__':
+    main()
+
+
