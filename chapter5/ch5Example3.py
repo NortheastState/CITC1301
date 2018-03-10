b42d5b3746d080f986253fccdec88ea65f70fc99
David Blair <dlblair@northeaststate.edu>
2018-03-10 14:05:00 -0500

    add string concatenation and repetition examples

---
+++ chapter5/ch5Example3.py
@@ -0,0 +1,19 @@
+# ===============================================================
+#
+#        Name: David Blair
+#        Date: 03/10/2018
+#      Course: CITC 1301
+#     Section: A70
+# Description: String concatenation and repetition
+#
+# ===============================================================
+
+
+def main():
+    var1 = "foo"
+    var2 = "bar"
+    print(var1 + var2)  # string concatenation "foobar"
+    print(var1 * 2)  # string repetition "foofoo"
+    print(var1 * 2 + var2 * 2)  # "foofoobarbar"
+
+main()
